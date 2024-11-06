db9a265cafe8ed41bc6758fd4c42b3641479489b
Dao Nguyen <zendobk@gmail.com>
2024-11-06 22:23:59 +0700

    load order-items

--- data/dump/sql-files/30_load_orders.sql
+++ data/dump/sql-files/30_load_orders.sql
@@ -4,3 +4,10 @@ load data infile '/docker-entrypoint-initdb.d/csv-files/orders.csv'
   enclosed by '"'
   lines terminated by '\n'
   ignore 1 rows;
+
+load data infile '/docker-entrypoint-initdb.d/csv-files/order-items.csv'
+  into table orders
+  fields terminated by ','
+  enclosed by '"'
+  lines terminated by '\n'
+  ignore 1 rows;
